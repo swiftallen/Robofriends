bad933eecec2454f990b1fb150b4689f4d1596fa
swiftallen <swiftallen@users.noreply.github.com>
2024-11-22 01:16:15 +0800

    Initial Commit

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({id, name, email}) => {
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            {}
+            <img src={`https://robohash.org/${id}?200x200`} alt='robots' /> {}
+            {}
+            <div>
+                <h2>{name}</h2>
+                <h2>{email}</h2>
+                {}
+            </div>
+        </div>
+    );
+}
+
+export default Card;
